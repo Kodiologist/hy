0ad0c239e94f1b2bac20986b97d7ecc01094880b
Kevin M Granger <git@kmg.fyi>
2026-05-03 11:52:04 -0400

    Fall back to builtins for target module in derive_target_module
    Co-authored-by: Kodi Arfer <git@arfer.net>

--- hy/macros.py
+++ hy/macros.py
@@ -118,7 +118,9 @@ def _same_modules(source_module, target_module):
 def derive_target_module(target_module, parent_frame):
     if target_module is None:
         target_namespace = parent_frame.f_globals
-        target_module = target_namespace.get("__name__", None)
+        target_module = target_namespace.get("__name__", "builtins")
+          # Why "builtins"? Because that's what Python produces with
+          # `exec("print(__name__)", globals = {})`.
     elif isinstance(target_module, str):
         target_module = importlib.import_module(target_module)
         target_namespace = target_module.__dict__
